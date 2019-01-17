d9f4d31aeb6889761aee0014a7f70feb6b75b4cc
Mo'ath Zaghdad <moath.m.zaghdad@gmail.com>
2019-01-17 19:45:30 +0200

    adding via a module

--- Notes-App/app.js
+++ Notes-App/app.js
@@ -1,8 +1,14 @@
+console.log('Starting app.js');
+
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes');
 
+console.log('Result: ', notes.add(8, 7));
+
+/*
 const user = os.userInfo();
-
 fs.appendFile('greetings.txt', `Hello ${user.username}!`, err => {
     if (err) console.log('Unable to write to file');
 });
+*/
