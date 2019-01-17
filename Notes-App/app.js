3b324afabebd74734dbd7c3d10ea825cf56620df
Mo'ath Zaghdad <moath.m.zaghdad@gmail.com>
2019-01-17 21:35:46 +0200

    wiring yargs to the notes methods

--- Notes-App/app.js
+++ Notes-App/app.js
@@ -2,6 +2,24 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes');
+const argv = yargs.argv;
 
+switch (argv._[0]) {
+    case 'read':
+        notes.getNote(argv.title);
+        break;
+    case 'remove':
+        notes.removeNote(argv.title);
+        break;
+    case 'add':
+        notes.addNote(argv.title, argv.body);
+        break;
+    case 'list':
+        notes.getAll();
+        break;
+    default:
+        console.log('Command not recognized');
+}
--- Notes-App/notes.js
+++ Notes-App/notes.js
@@ -1,1 +1,22 @@
 console.log('Starting notes.js');
+
+var addNote = (title, body) => {
+    console.log('adding title: ', title, ', body: ', body);
+};
+
+var getAll = () => {
+    console.log('Getting all notes');
+};
+
+var removeNote = title => {
+    console.log('Remove note: ', title);
+};
+var getNote = title => {
+    console.log('Getting note: ', title);
+};
+module.exports = {
+    addNote,
+    getAll,
+    removeNote,
+    getNote,
+};
