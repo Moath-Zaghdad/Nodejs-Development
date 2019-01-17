b2690377ad4af68ac0a2c25e887f333cdfb29e01
Mo'ath Zaghdad <moath.m.zaghdad@gmail.com>
2019-01-17 21:05:10 +0200

    cleaning

--- Notes-App/app.js
+++ Notes-App/app.js
@@ -1,20 +1,7 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes');
 
-console.log(_.isString(true));
-console.log(_.isString('Moath'));
-
-console.log(_.uniq(['a', 'A', 1, 2, 'a', 'A', 1, 1, 2]));
-
-// console.log('Result: ', notes.add(8, 7));
-
-/*
-const user = os.userInfo();
-fs.appendFile('greetings.txt', `Hello ${user.username}!`, err => {
-    if (err) console.log('Unable to write to file');
-});
-*/
