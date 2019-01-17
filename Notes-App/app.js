2bf0af41dcbf9d34c26a373e46861ce910bbec0c
Mo'ath Zaghdad <moath.m.zaghdad@gmail.com>
2019-01-17 19:57:05 +0200

    trying uniq and isString methods of lodash module

--- Notes-App/app.js
+++ Notes-App/app.js
@@ -2,9 +2,15 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes');
 
-console.log('Result: ', notes.add(8, 7));
+console.log(_.isString(true));
+console.log(_.isString('Moath'));
+
+console.log(_.uniq(['a', 'A', 1, 2, 'a', 'A', 1, 1, 2]));
+
+// console.log('Result: ', notes.add(8, 7));
 
 /*
 const user = os.userInfo();
