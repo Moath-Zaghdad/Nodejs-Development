d38fbbc7eab221ecb4e2a18bbc6b280837dbf43e
Mo'ath Zaghdad <moath.m.zaghdad@gmail.com>
2019-01-19 00:48:32 +0200

    creating a request to a website to get the *lat* and *lng* of a place by it's name

---
+++ Weather-App/app.js
@@ -0,0 +1,36 @@
+const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true,
+        },
+    })
+    .help()
+    .alias('help', 'h').argv;
+
+var encodedAddress = encodeURIComponent(argv.a);
+
+request(
+    {
+        url:
+            'http://www.mapquestapi.com/geocoding/v1/address?key=9MoCnoGDu4Sk1Swckb47QBn2xEkiaYAA&location=' +
+            encodedAddress,
+        json: true,
+    },
+    (err, responce, body) => {
+        if (err) console.log(`Unable to connect to MapQuestAPI`);
+        else if (body.info.statuscode !== 0)
+            console.log(`Unable to find the address`);
+        else {
+            //console.log(JSON.stringify(body, undefined, 4));
+            console.log(body.results[0].locations[0]);
+            console.log('');
+            console.log(body.results[0].locations[0].latLng);
+        }
+    },
+);
