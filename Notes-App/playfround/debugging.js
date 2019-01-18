78e08cfa283992acd93696cbf7fccc7bd626f106
Mo'ath Zaghdad <moath.m.zaghdad@gmail.com>
2019-01-18 02:14:12 +0200

    Adding notes about debugging in nodejs

---
+++ Notes-App/playfround/debugging.js
@@ -0,0 +1,29 @@
+var person = {
+    name: 'Moath',
+};
+
+debugger;
+person.age = 23;
+
+person.name = 'Zaghdad';
+/**
+ * $> run it via `node inspect playfround/debugging.js`
+ * or
+ * $> run it via `npx nodemon inspect playfround/debugging.js`
+ *
+ * to view more lines
+ * debug> list(10)
+ *
+ * to run the next statement
+ * debug> n
+ *
+ *
+ * to run the rest of the program untell the `debugger;` found
+ * debug> c
+ *
+ * to start the REPL – Read, Evaluate, Print & Loop mde
+ * debug> repl
+ *
+ * > person
+ * { name: 'Moath' }
+ */
