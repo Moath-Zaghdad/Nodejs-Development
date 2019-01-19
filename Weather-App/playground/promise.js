3bea248ef7d164377438645c4c3a18c35e83c93d
Mo'ath Zaghdad <moath.m.zaghdad@gmail.com>
2019-01-19 13:39:06 +0200

    passing arguments to a promise method

--- Weather-App/playground/promise.js
+++ Weather-App/playground/promise.js
@@ -26,3 +26,25 @@ somePromise
         console.log(`Error ${errorMessage}`);
     });
 
+/////////////////////////////////////////////////
+
+var asyncAdd = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (typeof a === 'number' && typeof b === 'number') resolve(a + b);
+            else reject('Arguments must be numbers');
+        }, 1500);
+    });
+};
+
+asyncAdd(5, 7)
+    .then(res => {
+        console.log('Result: ', res);
+        return asyncAdd(res, 33);
+    })
+    .then(res => {
+        console.log('Should be 45 ', res);
+    })
+    .catch(errorMessage => {
+        console.log('CATCH ', errorMessage);
+    });
