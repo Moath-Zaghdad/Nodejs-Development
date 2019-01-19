fed22181fbdd11ed1d9248911cf1264d3ecf891f
Mo'ath Zaghdad <moath.m.zaghdad@gmail.com>
2019-01-19 13:32:29 +0200

    then and catch

--- Weather-App/playground/promise-1.js
+++ Weather-App/playground/promise-1.js
@@ -6,10 +6,23 @@ var somePromise = new Promise((resolve, reject) => {
 });
 
 somePromise.then(
-    (message) => {
+    message => {
         console.log('Success: ', message);
     },
-    (errorMessage) => {
+    errorMessage => {
         console.log(`Error ${errorMessage}`);
     },
 );
+
+// or use catch to handle the errors
+//  this way is much better.
+//   when an error occurs we go to the catch directly
+//   and we don't continue with the then statements
+somePromise
+    .then(message => {
+        console.log('Success: ', message);
+    })
+    .catch(errorMessage => {
+        console.log(`Error ${errorMessage}`);
+    });
+
