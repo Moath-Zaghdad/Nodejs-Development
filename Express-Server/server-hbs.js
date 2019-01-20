41e304f1ad0cea1b46b3663eec0340d8f08667c5
Mo'ath Zaghdad <moath.m.zaghdad@gmail.com>
2019-01-20 02:48:31 +0200

    playing with handlebars

---
+++ Express-Server/server-hbs.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', text => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'welcome to our website!',
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+app.listen(3000);
